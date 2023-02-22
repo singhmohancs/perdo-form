649b701759a38b88ea607fdc987d3fb1ebc1dfc1
Mohan Singh <39476079+mohan@users.noreply.github.com>
2023-02-22 20:59:35 +0530

    fixed form issues

--- access/index.php
+++ access/index.php
@@ -1,31 +1,29 @@
-
 <?php
-  $ALLOWED_USERNAMES = ["mohan", "sameer"];
+/**
+ * Fuction is used to check allowed username and returns it if allowed
+ * 
+ * @return string|null
+ */
+function checkAndGetUsername()
+{
+    $ALLOWED_USERNAMES = ["mohan", "enrollnow"];
+    $username = "";
 
-  function hasAccess()
-  {
-      global $username;
-      $username = "";
+    if (isset($_GET["u"])) {
+        $username = $_GET["u"];
+    } else {
+        $uriSegments = explode(
+            "/",
+            parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH)
+        );
+        if (!empty($uriSegments[1])) {
+            $username = $uriSegments[1];
+        }
+    }
 
-      if (isset($_GET["u"])) {
-          $username = $_GET["u"];
-      } else {
-          $uriSegments = explode(
-              "/",
-              parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH)
-          );
-          if (!empty($uriSegments[1])) {
-              $username = $uriSegments[1];
-          }
-      }
-
-      if (in_array($username, $GLOBALS["ALLOWED_USERNAMES"])) {
-          return true;
-      } else {
-          return false;
-      }
-  }
-  ?>
-		
-	
-		 ?>
+    if (in_array($username, $ALLOWED_USERNAMES)) {
+        return $username;
+    } else {
+        return null;
+    }
+}
--- script.js
+++ script.js
@@ -1,52 +1,53 @@
+/** 
+ * This script is used to submit third party forms.
+ * You need to map the following fields to your form fields
+ * 	var formData = {
+			FNAME: form.fname,
+			LNAME: form.lname,
+			EMAIL: form.e6501,
+			username: form.username,
+		}
+*/
 
-$(document).ready(function() {
-	$('form').on('submit',function(e){
-		var requiredFieldNames = ['e6501', 'lname'];
-		var emailFieldName = 'e6501'; // text field name for email field if exist
-		var phoneFieldName = ''; // text field name for phone number if exist
-		var urls = [];
-		
-		// email validation
-		var emailValue = emailFieldName!= '' && $('input[name='+emailFieldName+']') ? $('input[name='+emailFieldName+']').val() : ''; 
-		
-		if(emailValue && emailValue != '') {
-			var isValidEmail = String(emailValue).toLowerCase()
-			.match(	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-			);
-			if(!isValidEmail) {
-				return true;
-			}
-		}
-		
-		// phone number validation for numeric and 10 digit
-		var phoneNumber = phoneFieldName!='' && $('input[name='+phoneFieldName+']') ? $('input[name='+phoneFieldName+']').val() : '';
-		if(phoneNumber && phoneNumber != '') {
-			phoneNumber = String(phoneNumber).replace(/\s/g, '').replace(/[()]/g, '').replace(/-/g, '');
-			if(isNaN(Number(phoneNumber)) || phoneNumber.length != 10) {
-				return true;
-			}
-		}
-		
-		var formArr = $(this).serializeArray();
-		var formObj = formArr.reduce(function(values, input) {
+$(document).ready(function () {
+
+	async function postData(url = '', data = {}) {
+		// Default options are marked with *
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: JSON.stringify(data) // body data type must match "Content-Type" header
+		});
+		return response.json(); // parses JSON response into native JavaScript objects
+	}
+
+	/** 
+	 * Bind submit event
+	*/
+	$('form').on('submit', function (e) {
+		var apiUrl = 'https://api.apify.com/v2/acts/heavenly_orca~submit-lead-to-livegood/runs?token=YFNhE2iCqt2YTDkfefHizvPAi';
+		var form = $(this).serializeArray();
+
+		form = form.reduce(function (values, input) {
 			values[input.name] = input.value;
 			return values;
 		}, {});
-		var hasAllRequiredFields = requiredFieldNames.every(function(field) {
-			return formObj[field] && formObj[field].trim() !== '';
-		})
-		if(hasAllRequiredFields) {
-			urls.forEach(function(url) {
-				$.ajax({
-					cache: false,
-					type: "POST",
-					url: url,
-					data: $(this).serialize(),
-					error: function (xhr, textStatus, msg) {},
-					success: function(msg) {}
-				});
-			})
+
+		var formData = {
+			FNAME: form.fname,
+			LNAME: form.lname,
+			EMAIL: form.e6501,
+			username: form.username,
 		}
-		return true;
-	})
+
+		postData(apiUrl, formData)
+			.then((data) => {
+				//console.log(data); // JSON data parsed by `data.json()` call
+				console.log('Form has been saved successfully');
+			});
+	});
+
 })
