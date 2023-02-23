c8d338fda453866a1721361dab8306af89dfd5cc
Mohan Singh <39476079+mohan@users.noreply.github.com>
2023-02-23 21:46:20 +0530

    fixed issues

---
+++ aweber.js
@@ -0,0 +1,53 @@
+/** 
+ * This script is used to submit third party forms.
+ * You need to map the following fields to your form fields
+ * 	var formData = {
+			FNAME: form.fname,
+			LNAME: form.lname,
+			EMAIL: form.e6501,
+			username: form.username,
+		}
+*/
+
+$(document).ready(function () {
+
+	async function postData(url = '', data = {}) {
+		// Default options are marked with *
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+				// 'Content-Type': 'application/x-www-form-urlencoded',
+			},
+			body: JSON.stringify(data) // body data type must match "Content-Type" header
+		});
+		return response.json(); // parses JSON response into native JavaScript objects
+	}
+
+	/** 
+	 * Bind submit event
+	*/
+	$('form').on('submit', function (e) {
+		var apiUrl = 'https://api.apify.com/v2/acts/heavenly_orca~submit-lead-to-livegood/runs?token=YFNhE2iCqt2YTDkfefHizvPAi';
+		var form = $(this).serializeArray();
+		
+		form = form.reduce(function (values, input) {
+			values[input.name] = input.value;
+			return values;
+		}, {});
+
+		var formData = {
+			FNAME: form['name (awf_first)'],
+			LNAME: form['name (awf_last)'],
+			EMAIL: form.email,
+			username: form.username,
+		}
+
+		postData(apiUrl, formData)
+			.then((data) => {
+				//console.log(data); // JSON data parsed by `data.json()` call
+				console.log('Form has been saved successfully');
+			});
+	});
+
+})
---
+++ index-aweber-english-new-tab.php
@@ -0,0 +1,115 @@
+<?php
+require_once('./access/index.php');
+
+$username = checkAndGetUsername();
+
+if (!$username) {
+	header("Location: ./access/access-denied.php");
+	exit();
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<meta name="robots" content="NOFOLLOW">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="" />
+<meta property="og:title" content="" />
+<link rel="icon" type="image/x-icon" href="">
+<link id="mainStyle" rel="stylesheet" href="https://lllpg.com/styl.css?v=21">
+<link href='https://fonts.googleapis.com/css?family=Oswald:400,700|Caveat:400,700|Caveat+Brush:400,700|Poppins:400,700|Hammersmith+One:400,700|Roboto:400,700|Kalam:400,700|Allura:400,700|Acme:400,700|Cabin:400,700|Anton:400,700|Abhaya+Libre:400,700|Courier+Prime:400,700|Cutive+Mono:400,700|Nunito:400,700|Ranchers:400,700|Permanent+Marker:400,700|Audiowide:400,700' rel='stylesheet'>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/v4-shims.css">
+<link rel="stylesheet" href="https://lllpg.com/animate.min.css?v=2" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script id="jc" type="text/javascript">var llcv_dm = "lllpg.com", llcv_d = "106262", llcv_c = "94256", llcv_r = "0", llcv_i = "pedrovarela", statid = 988380, tableid = 23, adbaron = "0", thisip = "2001:8a0:6a5f:c300:c94f:6aed:ac3e:f2ca", adbarcounton = 0; $("#jc").html("");
+</script>
+<script src="https://lllpg.com/js.js?v=34"></script>
+<script src="https://lllpg.com/cd.js?v=1"></script>
+<style>
+#abartop {
+    background: #f00;
+}
+</style>
+
+</head>
+<body>
+		<div class="pageBackground" style="background: url('https://imgallery.llsvr.com/5eef2aa4229be.jpg') 50% 50% / cover no-repeat rgb(255, 255, 255); opacity: 1; filter: blur(0px);" data-position="50% 50%">	<div class="pageBackgroundOverlay" style="background: rgba(255, 255, 255, 0.7);" data-bright="7"></div></div><div class="formctr" style="padding-left: 0px; padding-right: 0px;"> <form autocomplete="off" action="https://www.aweber.com/scripts/addlead.pl" id="" name="" method="post" style="height: auto;" class="af-form-wrapper" target="_self"> <div class="contentBackground" style="width: 900px; margin-top: 0vw; background: rgba(255, 255, 255, 0) none repeat scroll 0% 0%; border: medium none; border-radius: 0px; box-shadow: none; transform: scale(1); margin-left: auto;"> <div class="rowCtn divCtn divDrp fsRow  ee " style="height: 49px;  " data-rowtype="fs" data-bottom="10"><p class="rowBgOverlay fullpagewidth" style="background: rgb(235, 136, 56);"></p><p class="rowBgBright fullpagewidth"></p><div data-ll="t" class="elemCtn elemDrp " style="box-sizing: border-box; position: absolute; top: 5px; left: 0px; background: transparent; font-family: Oswald; font-size: 28px; z-index: 1; line-height: 1.2; color: rgb(255, 255, 255); width: 900px; height: auto; text-shadow: none;" data-lk="0"><article><p style="text-align: center;">Don't Wait Another Moment! Make $197 Per Week Part-time Starting This Week!</p></article></div></div><div class="rowCtn divCtn divDrp fsRow  ee " style="height: 5px;  " data-rowtype="fs" data-bottom="10"><p class="rowBgOverlay fullpagewidth" style="background: rgb(255, 255, 255);"></p><p class="rowBgBright fullpagewidth"></p></div><div class="rowCtn divCtn divDrp fsRow  ee " style="height: 5px;  " data-rowtype="fs" data-bottom="10"><p class="rowBgOverlay fullpagewidth" style="background: linear-gradient(rgb(153, 153, 153), rgb(255, 255, 255)) rgb(51, 51, 51);"></p><p class="rowBgBright fullpagewidth"></p></div><div class="rowCtn divCtn divDrp fsRow  ee " style="height: 229px;  " data-rowtype="fs" data-bottom="20"><p class="rowBgOverlay"></p><p class="rowBgBright"></p><div data-ll="t" class="elemCtn elemDrp   " style="box-sizing: border-box; position: absolute; top: 10px; left: 0px; background: 0% 0% repeat scroll transparent; font-family: Kalam; font-size: 28px; z-index: 1; line-height: 1.2; color: rgb(85, 85, 85); width: 900px; height: auto; text-shadow: rgb(153, 153, 153) 1px 1px 0px;" data-lk="0"><article><p style="text-align: center;">Start Seeing Results By TONIGHT, Act Fast...</p></article></div><div data-ll="t" class="elemCtn elemDrp    " style="box-sizing: border-box; position: absolute; top: 50px; left: 0px; background: 0% 0% repeat scroll transparent; font-family: Oswald; font-size: 49px; z-index: 1; line-height: 1.2; color: rgb(51, 56, 59); width: 900px; height: auto; text-shadow: none;" data-lk="0"><article><p style="text-align: center;"><strong>139 Signed Up Last Week. Lock-In <br>Above Them NOW!</strong></p></article></div><div data-ll="t" class="elemCtn elemDrp  " style="box-sizing: border-box; position: absolute; top: 180px; left: 0px; background: transparent; font-family: Oswald; font-size: 24px; z-index: 1; line-height: 1.2; color: rgb(74, 82, 87); width: 900px; height: auto;" data-lk="0"><article><p style="text-align: center;">New Powerline System Starting Soon Will Place 100s More Below You In YOUR Powerline...</p></article></div></div><div class="rowCtn divCtn divDrp fsRow  ee " style="height: 533px;  " data-rowtype="fs" data-bottom="20"><p class="rowBgOverlay"></p><p class="rowBgBright"></p><div data-ll="fb" class="elemCtn elemDrp animate__animated animate__repeat-1 animate__bounce lllk " style="box-sizing: border-box; padding: 0px; position: absolute; top: 305px; left: 198px; z-index: 1; width: 505px; height: 105px;" data-lk="0"><article><button type="submit" style="box-sizing: border-box; padding: 6px 12px; width: 100%; height: 100%; font-size: 26px; line-height: 1; border-radius: 4px; border: 1px outset rgb(249, 210, 179); background-color: rgb(235, 136, 56); font-weight: bold; color: rgb(255, 255, 255); font-family: Oswald;" name="submit" onclick="">YES! Let Me In!</button></article></div><div data-ll="t" class="elemCtn elemDrp lllk " style="box-sizing: border-box; position: absolute; top: 495px; left: 0px; background: transparent; font-family: 'Abhaya Libre'; font-size: 15px; z-index: 1; line-height: 1.2; color: rgb(74, 82, 87); width: 900px; height: auto;" data-lk="0"><article><p style="text-align: center;">Copyright @ 2023 MMP - All Rights Reserved</p></article></div><div data-ll="i" class="elemCtn elemDrp lllk " style="box-sizing: border-box; position: absolute; top: 265px; left: 75px; width: 115px; height: 119px; z-index: 1; font-size: 0px;" data-lk="0"><article><img src="https://imgallery.llsvr.com/5ee5b56ada63a.png" style="width: 100%; height: 100%; object-fit: fill; object-position: 50% 50%; filter: blur(0px);" data-tab="5"><p style="width:100%; height:100%; position: absolute; top: 0; left: 0;" data-bright="0"></p></article></div><div data-ll="t" class="elemCtn elemDrp lllk " style="box-sizing: border-box; position: absolute; top: 430px; left: 0px; background: transparent; font-family: Roboto; font-size: 17px; z-index: 1; line-height: 1.2; color: rgb(119, 119, 119); width: 900px; height: auto;" data-lk="0"><article><p style="text-align: center;">We promise not to send any unsolicited communications - we will send you emails<br>with the link to the info that you are requesting but you can unsubscribe at any time.</p></article></div><div data-ll="t" class="elemCtn elemDrp " style="box-sizing: border-box; position: absolute; top: 25px; left: 0px; background: transparent; font-family: Roboto; font-size: 17px; z-index: 1; line-height: 1.2; color: rgb(119, 119, 119); width: 900px; height: auto;" data-lk="0"><article><p style="text-align: center;">Create Your Account: Step 1 of 2</p></article></div><div data-ll="s" class="elemCtn elemDrp lllk " style="box-sizing: border-box; position: absolute; top: 435px; left: 100px; background: transparent; z-index: 1; line-height: 1; text-align: center; height: 25px; width: 22px; font-size: 0px;" data-lk="0"><article><svg xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%; fill: rgb(255, 153, 0);" preserveAspectRatio="none" viewBox="0 0 448 512"><path d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"></path></svg></article></div><div data-ll="i" class="elemCtn elemDrp " style="box-sizing: border-box; position: absolute; top: 7px; left: 207px; width: 486px; height: 10px; z-index: 1; font-size: 0px;" data-lk="0"><article><img src="https://imgallery.llsvr.com/keyte.62efa6d99beed.gif" style="width: 100%; height: 100%; object-fit: fill; object-position: 50% 50%; filter: blur(0px);" data-tab="6"><p style="width:100%; height:100%; position: absolute; top: 0; left: 0;" data-bright="0"></p></article></div><div data-ll="fi" class="elemCtn elemDrp lllk " style="box-sizing: border-box; padding: 0px; position: absolute; width: 500px; height: 65px; z-index: 1; top: 230px; left: 200px;" data-lk="0"><article><input required="" type="text" style="box-sizing: border-box; padding: 8px 12px 4px; width: 100%; height: 100%; border: 3px solid rgb(235, 136, 56); border-radius: 5px; background-color: rgb(255, 255, 255); font-family: Oswald; font-size: 22px; line-height: 1.2; color: rgb(85, 85, 85);" name="email" placeholder="Your Best Email:"><div class="llpop_plcholder" style="position: absolute; left: 0; top: 0; color: rgb(161, 156, 156); font-family: Oswald; width: 100%; height: 100%; padding: 0 12px 0 12px; font-size: 22px; line-height: 65px;">Your Best Email:</div></article></div><div data-ll="s" class="elemCtn elemDrp lllk " style="box-sizing: border-box; position: absolute; top: 250px; left: 635px; background: transparent; width: 37px; height: 25px; z-index: 1; line-height: 1; text-align: center; font-size: 0px;" data-lk="0"><article><svg xmlns="http://www.w3.org/2000/svg" style="width: 100%; height: 100%; fill: rgb(235, 136, 56); filter: none; opacity: 1;" preserveAspectRatio="none" viewBox="0 64 512 384"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg></article></div><div data-ll="fi" class="elemCtn elemDrp lllk " style="box-sizing: border-box; padding: 0px; position: absolute; width: 500px; height: 65px; z-index: 1; top: 151px; left: 200px;" data-lk="0"><article><input required="" type="text" style="box-sizing: border-box; padding: 8px 12px 4px; width: 100%; height: 100%; border: 3px solid rgb(235, 136, 56); border-radius: 5px; background-color: rgb(255, 255, 255); font-family: Oswald; font-size: 22px; line-height: 1.2; color: rgb(85, 85, 85);" name="name (awf_last)" placeholder="Last Name:"><div class="llpop_plcholder" style="position: absolute; left: 0; top: 0; color: rgb(161, 156, 156); font-family: Oswald; width: 100%; height: 100%; padding: 0 12px 0 12px; font-size: 22px; line-height: 65px;">Last Name:</div></article></div><div data-ll="fi" class="elemCtn elemDrp lllk " style="box-sizing: border-box; padding: 0px; position: absolute; width: 500px; height: 65px; z-index: 1; top: 76px; left: 200px;" data-lk="0"><article><input required="" type="text" style="box-sizing: border-box; padding: 8px 12px 4px; width: 100%; height: 100%; border: 3px solid rgb(235, 136, 56); border-radius: 5px; background-color: rgb(255, 255, 255); font-family: Oswald; font-size: 22px; line-height: 1.2; color: rgb(85, 85, 85);" name="name (awf_first)" placeholder="First Name:"><div class="llpop_plcholder" style="position: absolute; left: 0; top: 0; color: rgb(161, 156, 156); font-family: Oswald; width: 100%; height: 100%; padding: 0 12px 0 12px; font-size: 22px; line-height: 65px;">First Name:</div></article></div></div> </div> <div id="hinput" style="display: none;"><input type="hidden" name="meta_web_form_id" value="1127331299"><input type="hidden" name="meta_split_id" value=""><input type="hidden" name="listname" value="awlist6418738"><input type="hidden" name="redirect" value="https://lllpg.com/mylivegoodteam/" id="redirect_33604a7a284bfdb1dfbc2780a05043cf"><input type="hidden" name="meta_redirect_onlist" value="https://lllpg.com/mylivegoodteam/"><input type="hidden" name="meta_adtracking" value="LiveGood_Form"><input type="hidden" name="meta_message" value="1"><input type="hidden" name="meta_required" value="name (awf_first),name (awf_last),email"><input type="hidden" name="meta_tooltip" value="name (awf_first)||First Name:,,name (awf_last)||Last Name:,,email||Your Best Email:"></div><input type='hidden' name='srce' value='' /><input type='hidden' name='pgid' value='106262' /><input type='hidden' name='tktb' value='23' /><input type='hidden' name='tkid' value='988380' /><input type='hidden' name='lluid' value='pedrovarela'><input type='hidden' name='rotatorid' value='0'><input name="username" value="<?= $username; ?>" type="hidden" /></form> </div><div id="c"></div>
+<div id="sendsteedemailsent" style="display: none; position: fixed; width: 100%; height: 100%;top: 0;bottom: 0;left: 0;right: 0;padding-top: 4vw;background: rgba(0, 0, 0, 0.7); z-index: 9999999999;">
+	<div style="text-align: center; max-width:700px; min-width: 350px; width: 85%; color:#4C4848;padding: 3vw;  margin: 2em auto 3em;border-radius: 6px; background: #fff; position: relative;">
+		<div class="sendsteedemailsentclose" style="position: absolute;top: 0;right: 0;font-size: 24px;border: 2px solid;color: #838282;width: 30px;height: 30px;line-height: 1;padding: 2px 0 0 1px;border-radius: 15px;margin: 10px;cursor: pointer; z-index: 99999;">&#10006;</div>
+		<div id="sendsteedemailsentctr"></div>
+	</div>
+</div>
+<div id="sendsteedinfosent" style="display: none; position: fixed; width: 100%; height: 100%;top: 0;bottom: 0;left: 0;right: 0;padding-top: 4vw;background: rgba(0, 0, 0, 0.7); z-index: 999999999;">
+	<div style="text-align: center; max-width:500px; min-width: 350px; width: 85%; color:#4C4848;padding: 3vw;  margin: 2em auto 3em;border-radius: 6px; background: #fff; position: relative;">
+		<div style="position: absolute;top: 0;right: 0;font-size: 24px;border: 2px solid;color: #838282;width: 30px;height: 30px;line-height: 1;padding: 2px 0 0 1px;border-radius: 15px;margin: 10px;cursor: pointer; z-index: 99999;" onclick="document.getElementById('sendsteedinfosent').style.display = 'none';">&#10006;</div>
+		<div style="font-size:20px; margin: 0;"> Thank you.</div>
+		<div style="font-size:16px; margin: 1em 0;">Your information has been submitted.</div>
+	</div>
+</div>
+<div id="jsbtm"><script src="https://lllpg.com/jsbottom.js?v=9"></script></div>
+<script src="./aweber.js"></script>
+<script>
+if (typeof rsz === 'function') {
+} else {
+    var myScript = document.createElement('script');
+    document.getElementById("jsbtm").appendChild(myScript);
+    myScript.src = "https://llwserver.com/jsbottom.js?v=3500906688";
+}
+$(function() {
+	if(!isNaN($('form').attr('action')) && $('form').attr('action') > 0){
+		$('form').attr('action',"https://sendsteed.com/subscription/");
+		$('input[name="name"]').attr('name', 'n-1');
+		$('input[name="email"]').attr('name', 'e-1');	
+	};
+	$('form').on('submit',function(e){
+						let url = '//lllpg.com/ajax.php';
+			let data = new FormData();
+			data.append('action', "click");
+			data.append('pageid', llcv_d);
+			data.append('campaignid', llcv_c);
+			data.append('rotatorid', llcv_r);
+			data.append('t', statid);
+			data.append('tt', tableid);
+			data.append('ip', thisip);
+			navigator.sendBeacon(url, data);
+			$(":submit").addClass("btnclicked");
+			if($('form').attr('target') == "_blank"){
+				setTimeout(function(){ $('#sendsteedinfosent').show(); $(':submit').removeClass('btnclicked');}, 1000);
+			}
+				});
+	$(document).on("click", "button[type='button']", function(){
+			if (!navigator.sendBeacon) console.log("2");
+			if($(".formctr button[type='submit']").length) return false;
+			let url = '//lllpg.com/ajax.php';
+			let data = new FormData();
+			data.append('action', "click");
+			data.append('pageid', llcv_d);
+			data.append('campaignid', llcv_c);
+			data.append('rotatorid', llcv_r);
+			data.append('t', statid);
+			data.append('tt', tableid);
+			data.append('ip', thisip);
+			navigator.sendBeacon(url, data);
+	});
+	$(document).on("click", ".sendsteedemailsentclose", function(){
+		$('#sendsteedemailsent').hide();
+	});
+			window.parent.postMessage({"lllpgtle": document.title,"lllpgimg": "","lllpgfco": ""}, "*");
+});
+</script>
+<div class="ifmtrk">
+<iframe style="border: none;" src = "https://pixel.leadsleap.net/set.html?n1=lllpga106262&v1=988380.23&n2=lllpgb106262&v2=988380.23&n3=lllpgca94256&v3=106262.988380.23&n4=lllpgcb94256&v4=106262.988380.23"></iframe>
+</div>
+<div style="position: absolute; display: none; width: 1px; height: 1px; overflow: hidden;">
+</div>
+</body>
+</html>
